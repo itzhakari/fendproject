8c42f03ce0fcadff1b51a0535eead31bffbb36a3
itzhakari <hakarimida@gmail.com>
2024-09-24 22:10:27 +0600

    Did nothing

--- fe-project/src/Navbar.jsx
+++ fe-project/src/Navbar.jsx
@@ -1,13 +1,13 @@
+
 function Navbar() {
-    return (
-      <div className="navbar">
-        <button className="home">Home</button>
-        <button className="explore">Explore</button>
-        <button className="mens">Men's</button>
-        <button className="womens">Women's</button>
-      </div>
-    );
-  }
-  
-  export default Navbar;
-  
+  return (
+    <div className="navbar">
+      <button className="home">Home</button>
+      <button className="explore">Explore</button>
+      <button className="mens">Men's</button>
+      <button className="womens">Women's</button>
+    </div>
+  );
+}
+
+export default Navbar;
